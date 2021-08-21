e4ee9fc0b763f1ecde75c879c3a71b42844c58d5
Vladislav Flicker <flickervladislav@gmail.com>
2021-08-21 16:33:29 +0300

    Add useCallback and useMemo for planet-info component

--- src/components/planet-info/planet-info.js
+++ src/components/planet-info/planet-info.js
@@ -1,28 +1,60 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback, useMemo } from 'react';
 import './planet-info.css';
 
-const usePlanetInfo = (id) => {
-  const [planetName, setPlanetName] = useState(null);
+const getPlanet = async (id) => {
+  const res = await fetch(`http://swapi.dev/api/planets/${id}/`);
+  return await res.json();
+};
+
+const useRequest = (request) => {
+  const initialState = useMemo(() => ({
+    data: null,
+    loading: true,
+    error: null,
+  }), []);
+
+  const [ dateState, setDateState ] = useState(initialState);
 
   useEffect(() => {
     let cancelled = false;
 
-    fetch(`http://swapi.dev/api/planets/${id}/`)
-      .then((res) => res.json())
-      .then((planet) => !cancelled && setPlanetName(planet.name));
+    setDateState(initialState)
+
+    request()
+      .then((data) => !cancelled && setDateState({
+        data,
+        loading: false,
+        error: null,
+      }))
+      .catch((error) => !cancelled && setDateState({
+        data: null,
+        loading: false,
+        error,
+      }));
 
     return () => cancelled = true;
-  }, [id]);
+  }, [ request, initialState ]);
 
-  return planetName;
+  return dateState;
+};
+
+const usePlanetInfo = (id) => {
+  const request = useCallback(() => getPlanet(id), [ id ]);
+  return useRequest(request);
 };
 
 const PlanetInfo = ({ id }) => {
-  const planetName = usePlanetInfo(id);
+  const { data, loading, error } = usePlanetInfo(id);
 
-  return (
-    <div>{id} - {planetName}</div>
-  )
+  if (error) {
+    return <div>Something is wrong</div>;
+  }
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  return <div>{id} - {data && data.name}</div>;
 };
 
 export default  PlanetInfo;
