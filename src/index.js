07dbf5beaa1c11117fb56dc4334cc8dcf39a2fc4
Vladislav Flicker <flickervladislav@gmail.com>
2021-08-23 16:18:32 +0300

    Add reducer

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
+import './reducer';
 import './index.css';
-import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
---
+++ src/reducer.js
@@ -0,0 +1,17 @@
+const reducer = (state = 0, action) => {
+  switch (action.type) {
+    case 'INC':
+      return state + 1;
+
+    default:
+      return state;
+  }
+};
+
+let state = reducer(undefined, {});
+
+state = reducer(state, {type: 'INC'});
+console.log(state)
+
+state = reducer(state, {type: 'INC'});
+console.log(state)
